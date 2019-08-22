21900a4e065c8ed5691d8c1b82d5e75fcd4a0053
Nineth! <41068598+ninethbacovic@users.noreply.github.com>
2019-08-22 10:27:58 -0300

    Added UsersDetailView and AnimalsDetailView

--- app_pets/serializers.py
+++ app_pets/serializers.py
@@ -6,7 +6,31 @@ class UsersSerializer(ModelSerializer):
     model = Users
     fields = ('username', 'email', 'name', 'created', 'modified')
 
+class UsersDetailSerializer(ModelSerializer):
+  class Meta:
+    model = Users
+    fields = ('username', 'email', 'name', 'created', 'modified')
+
+
 class AnimalsSerializer(ModelSerializer):
+    class Meta:
+        model = Animals
+        fields = ['contact',
+         'name',
+        'description',
+         'image',
+          'age',
+          'color',
+          'gender',
+          'specie',
+          'size',
+          'category',
+          'location',
+          'city',
+          'state',
+          ]
+
+class AnimalsDetailSerializer(ModelSerializer):
     class Meta:
         model = Animals
         fields = ['contact',
